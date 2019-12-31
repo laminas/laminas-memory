80ebf142c3135e8828262d3897d49e8ba6eed27f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:24:47 -0600

    Rewriting as Laminas Project package

--- test/LockedTest.php
+++ test/LockedTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-memory for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-memory/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-memory/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Memory;
+namespace LaminasTest\Memory;
 
-use Zend\Memory\Container;
+use Laminas\Memory\Container;
 
 /**
- * @group      Zend_Memory
+ * @group      Laminas_Memory
  */
 class LockedTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,7 +22,7 @@ class LockedTest extends \PHPUnit_Framework_TestCase
     {
         $memObject = new Container\Locked('0123456789');
 
-        $this->assertInstanceOf('Zend\Memory\Container\Locked', $memObject);
+        $this->assertInstanceOf('Laminas\Memory\Container\Locked', $memObject);
     }
 
     /**
