df53bdb822f6fc6ddbfeb69c6354b5b4a082698d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:24:40 -0600

    Rewriting as Laminas Project package

--- test/ValueTest.php
+++ test/ValueTest.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-memory for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-memory/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-memory/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Memory;
+namespace LaminasTest\Memory;
 
-use Zend\Memory;
-use Zend\Memory\Container;
+use Laminas\Memory;
+use Laminas\Memory\Container;
 
 /**
- * @group      Zend_Memory
+ * @group      Laminas_Memory
  */
 class ValueTest extends \PHPUnit_Framework_TestCase
 {
